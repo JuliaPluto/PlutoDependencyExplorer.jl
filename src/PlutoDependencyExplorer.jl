d7b3fb0c30ef96a7c1edb42fe2a8f7d0b0af744e
Fons van der Plas <fonsvdplas@gmail.com>
2024-01-21 10:54:20 +0100

    module docstring

--- src/PlutoDependencyExplorer.jl
+++ src/PlutoDependencyExplorer.jl
@@ -1,3 +1,7 @@
+
+"""
+This package contains Pluto's dependency sorting algorithm. Given a list of cell codes, PlutoDependencyExplorer can tell you in which order these cells should run.
+"""
 module PlutoDependencyExplorer
 
 using ExpressionExplorer
