af7b69931998a89a0661110c9e299a296835f5d3
Fons van der Plas <fonsvdplas@gmail.com>
2024-01-30 11:47:15 +0100

    yolo

--- test/pluto integration/run.jl
+++ test/pluto integration/run.jl
@@ -3,7 +3,14 @@ import Pkg
 pkg_source = include("./DEV EDIT ME pluto pkg source.jl")
 
 if pkg_source !== nothing
-    if pkg_source.rev === nothing
+    
+    is_dev = if hasfield(typeof(pkg_source), :rev)
+        pkg_source.rev === nothing
+    else
+        pkg_source.repo.rev === nothing
+    end
+    
+    if is_dev
         Pkg.develop(pkg_source)
     else
         Pkg.add(pkg_source)
